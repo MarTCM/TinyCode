46eff49f8effe2e8597a0769b1d376d76b5b3a1b
Marwane ElBaraka <marwane.elbaraka@gmail.com>
2026-04-05 12:05:09 +0100

    update main.py to accept user input for prompt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,12 +11,16 @@ if (api_key == None):
 
 client = genai.Client(api_key=api_key)
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
 def main():
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=args.user_prompt
     )
-    print("User prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print("User prompt: " + args.user_prompt)
     if (response.usage_metadata != None):
         print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
         print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
