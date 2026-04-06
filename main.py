31a9439551f024476395e1df28c9deaaf8e81391
Marwane ElBaraka <marwane.elbaraka@gmail.com>
2026-04-06 13:30:58 +0100

    add verbose argument to main.py to choose response depth

--- main.py
+++ main.py
@@ -14,25 +14,30 @@ client = genai.Client(api_key=api_key)
 def main():
     parser = argparse.ArgumentParser(description="AI Code Agent")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     messages = [genai.types.Content(role="user", parts=[genai.types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
 
-    generate_content(client, messages)
+    generate_content(client, messages, args.verbose)
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages
     )
 
-    if (response.usage_metadata != None):
-        print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-        print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
-        print("Response:")
-        print(response.text)
-    else:
-        raise RuntimeError("API call failed")
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    print("Response:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
